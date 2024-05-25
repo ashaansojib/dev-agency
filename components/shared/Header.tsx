5435a7211a39c341a2ad408cd88d53949b7ce54f
ashaansojib <ashaduzzamansojib67@gmail.com>
2024-05-25 17:14:44 +0600

    change transition

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,45 +1,40 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 
 const Header = () => {
-    const [scrollBg, setScrollBg] = useState(false);
-    const handleScroll = () => {
-        if (window.scrollY > 0) {
-            setScrollBg(true);
-        } else {
-            setScrollBg(false);
-        }
-    };
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll);
-        return () => window.removeEventListener('scroll', handleScroll);
-    }, []);
+  const [scrollBg, setScrollBg] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 70) {
+      setScrollBg(true);
+    } else {
+      setScrollBg(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className={`w-full top-0 z-10 transition duration-500 ${scrollBg ? "bg-accent sticky text-primary" : "absolute text-accent"}`}>
+    <header
+      className={`w-full top-0 z-10 transition-transform duration-500 ease-in-out ${
+        scrollBg ? "bg-accent sticky text-primary transform-none" : "absolute text-accent"
+      }`}
+    >
       <div className="my-container h-[70px] flex justify-between items-center">
         <h1 className="font-semibold text-xl">Dev.co</h1>
         <nav className="flex gap-4 justify-between">
-          <ul>
-            <Link className="nav-link" href="/">
-              Home
-            </Link>
-            <Link className="nav-link" href="/">
-              Our Work
-            </Link>
-            <Link className="nav-link" href="/">
-              Services
-            </Link>
-            <Link className="nav-link" href="/">
-              About US
-            </Link>
-            <Link className="nav-link" href="/">
-              Blog
-            </Link>
+          <ul className="flex gap-4">
+            <li><Link className="nav-link" href="/">Home</Link></li>
+            <li><Link className="nav-link" href="/">Our Work</Link></li>
+            <li><Link className="nav-link" href="/">Services</Link></li>
+            <li><Link className="nav-link" href="/">About Us</Link></li>
+            <li><Link className="nav-link" href="/">Blog</Link></li>
           </ul>
-          <Link className={`${scrollBg ? "my-btn" : "hero-btn"}`} href="/">
-            Contact
-          </Link>
+          <Link className={`btn ${scrollBg ? "my-btn" : "hero-btn"}`} href="/">Contact</Link>
         </nav>
       </div>
     </header>
